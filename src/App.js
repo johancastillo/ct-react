66d6f135ebeccd4dcf51452366734f96f694b219
Johan Castillo <jcjohan2707@gmail.com>
2020-11-15 11:52:10 -0400

    Data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navegation from './components/Navegation';
+
+import {todos} from './datos.json';
+
+console.log(todos);
+
+class App extends Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return (
+      <div>
+        <Navegation titulo="Holaaaa"/>
+      </div>
+    );
+  }
 }
 
 export default App;
