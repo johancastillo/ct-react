f5e468dd6b2f32d8b9ab0bcbb3a3c467639b6dba
Johan Castillo <jcjohan2707@gmail.com>
2020-11-15 13:51:31 -0400

    List tasks

--- src/App.js
+++ src/App.js
@@ -5,17 +5,49 @@ import Navegation from './components/Navegation';
 
 import {todos} from './datos.json';
 
-console.log(todos);
-
 class App extends Component {
   constructor(){
     super();
+    this.state = {
+      todos,
+      ntasks: todos.length
+    }
   }
 
   render(){
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4">
+          <div className="card mt-4">
+          <div className="card-header">
+            <h3> {todo.title} </h3>
+
+            <span className="badge badge-pill badge-danger ml-2">
+              {todo.priority}
+            </span>
+          </div>
+
+          <div className="card-body">
+            <p>{todo.description}</p>
+            <p>
+              <mark>{todo.responsible}</mark>
+            </p>
+          </div>
+        </div>
+        </div>
+      )
+    })
+
     return (
       <div>
-        <Navegation titulo="Holaaaa"/>
+        <Navegation ntasks={this.state.ntasks} />
+
+        <div className="container">
+          <div className="row mt-4">
+          {todos}
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/components/Navegation.js
+++ src/components/Navegation.js
@@ -3,9 +3,15 @@ import React, {Component} from 'react';
 class Navegation extends Component{
     render(){
         return (
-            <h1>
-                {this.props.titulo}
-            </h1>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">
+                    Tasks List
+
+                    <span className="badge badge-pill badge-light ml-2">
+                        {this.props.ntasks}
+                    </span>
+                </a>
+            </nav>
         )
     }
 }
